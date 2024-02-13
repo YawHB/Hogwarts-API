8d0b8a991352fc5fef6c1a53b0eba50d656c3d54
YawHB <yawhboateng@gmail.com>
2024-02-13 22:37:26 +0100

    Created get single student route

--- src/main/java/edu/hogwarts/controllers/StudentController.java
+++ src/main/java/edu/hogwarts/controllers/StudentController.java
@@ -3,9 +3,11 @@ package edu.hogwarts.controllers;
 import edu.hogwarts.models.Student;
 import edu.hogwarts.repositories.StudentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
@@ -24,6 +26,15 @@ public class StudentController {
 
 
     }
+
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id) {
+        Optional<Student> student =  studentRepository.findById(id); //We use Optional in order to be able to handle if there is no match by id
+        return student.orElse(null); // orElse betyder: returnér objektet hvis det findes, ellers returnér det der står i parentesen ("null" i voes tilfælde).
+
+    }
+
+
 
 
 }
