e57b9c4e732cefae9e5b8ea7070b267475bdd67c
YawHB <yawhboateng@gmail.com>
2024-02-14 08:10:48 +0100

    Improved response from get single student

--- src/main/java/edu/hogwarts/controllers/StudentController.java
+++ src/main/java/edu/hogwarts/controllers/StudentController.java
@@ -2,8 +2,10 @@ package edu.hogwarts.controllers;
 
 import edu.hogwarts.models.Student;
 import edu.hogwarts.repositories.StudentRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,12 +30,17 @@ public class StudentController {
     }
 
     @GetMapping("/students/{id}")
-    public Student getStudent(@PathVariable int id) {
+    public ResponseEntity<Student> getStudent(@PathVariable int id) { //ResponseEntity bruges til at pakke svaret (enten studerendeobjektet eller en fejlmeddelelse) sammen med den tilsvarende HTTP-statuskode.
         Optional<Student> student =  studentRepository.findById(id); //We use Optional in order to be able to handle if there is no match by id
-        return student.orElse(null); // orElse betyder: returnér objektet hvis det findes, ellers returnér det der står i parentesen ("null" i voes tilfælde).
+        return ResponseEntity.of(student); //Tjekker om person objektet findes. Gør den det, returneres et RespinseEntity med OK 200. Gør den ikke returneres end 404 NOT FOUND
+
 
     }
 
+    @PostMapping("/students")
+    public Student createStudent(Student student) {
+
+    }
 
 
 
